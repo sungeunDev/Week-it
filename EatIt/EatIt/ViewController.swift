df652125749eb96fba3622ef4a2cc0877f47977e
sungeunDev <starfg62@gmail.com>
2018-05-16 13:02:19 +0900

    [project build]

---
+++ EatIt/EatIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EatIt
+//
+//  Created by sungnni on 2018. 5. 16..
+//  Copyright © 2018년 AppKings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
