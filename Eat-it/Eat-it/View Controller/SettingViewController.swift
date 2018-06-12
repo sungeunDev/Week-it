038ad8c3e29681ed0c6479536e7ff4ba02893165
sungeunDev <starfg62@gmail.com>
2018-06-12 16:37:39 +0900

    [feat] Change month with swipe gesture in MainVC
    -
    
    -

--- Eat-it/Eat-it/View Controller/SettingViewController.swift
+++ Eat-it/Eat-it/View Controller/SettingViewController.swift
@@ -8,13 +8,25 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
 }
 
-//extension SettingViewController: UITableView
+
+extension SettingViewController {
+    
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+    
+    
+}
+
